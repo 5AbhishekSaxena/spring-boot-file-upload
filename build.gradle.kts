fdcd8f29061d8e8ac2e49e1487e9d88f8357ef83
Abhishek Saxena <5abhisheksaxena@gmail.com>
2021-11-12 02:13:58 +0530

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.5.6"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.31"
+	kotlin("plugin.spring") version "1.5.31"
+}
+
+group = "tech.developingdeveloper"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web:2.5.6")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("com.google.guava:guava:31.0.1-jre")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/tech/developingdeveloper/fileuploadplayground/FileUploadPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package tech.developingdeveloper.fileuploadplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileUploadPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileUploadPlaygroundApplication>(*args)
+}
